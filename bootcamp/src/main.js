577139f3dcd3314f6427ba03e35c36bb7f403017
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-03-05 13:56:19 -0500

    [BC-02] Rename routes and update 'App' import

--- bootcamp/src/main.js
+++ bootcamp/src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import '@mdi/font/css/materialdesignicons.css';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify';
+import App from '@/app/app.vue';
+import router from '@/router';
+import store from '@/store';
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
