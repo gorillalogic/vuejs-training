b4b0818d315638df9c0e845e8f8a4ed4dca8125e
Alejandro Benjumea <59701575+alejandro-benjumea-gorilla-logic@users.noreply.github.com>
2020-03-09 11:23:31 -0500

    [BC-06] Add 'Player' component (#6)
    * [BC-06] Add 'Player' component
    
    * [BC-06] Refactor 'MainLayout' component

---
+++ bootcamp/src/components/player/constants.js
@@ -0,0 +1,2 @@
+export const UPDATE_INTERVAL = 1000;
+export const TIME_OFFSET_10 = 10;
---
+++ bootcamp/src/components/player/player.vue
@@ -0,0 +1,3 @@
+<template src="./player.html" />
+<script src="./player.js" />
+<style lang="scss" src="./player.scss" scoped />
