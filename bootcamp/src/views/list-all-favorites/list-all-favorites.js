2cad80ea76665eec3378edeb8d4dd08c8f7c31f5
Alejandro Benjumea <59701575+alejandro-benjumea-gorilla-logic@users.noreply.github.com>
2020-03-09 15:18:28 -0500

    [BC-12] Add podcasts and favorites pages (#12)
    * [BC-12] Refactor 'ListAllFavorites' component
    
    * [BC-12] Refactor 'ListAllPodcast' component

--- bootcamp/src/views/list-all-favorites/list-all-favorites.js
+++ bootcamp/src/views/list-all-favorites/list-all-favorites.js
@@ -1,3 +1,26 @@
+import { filterBySearchTerm } from '@/utils/filterBySearchTerm';
+import api from '@/api';
+import TrackList from '@/components/track-list/track-list.vue';
+
+const MAX_LIST_ITEMS = 100;
+
 export default {
   name: 'ListAllFavorites',
+  components: {
+    TrackList,
+  },
+  data() {
+    return {
+      favorites: [],
+      searchTerm: '',
+    };
+  },
+  computed: {
+    favoriteList() {
+      return filterBySearchTerm(this.searchTerm, this.favorites);
+    },
+  },
+  async mounted() {
+    this.favorites = await api.getFavoritesTracks(MAX_LIST_ITEMS);
+  },
 };
--- bootcamp/src/views/list-all-podcast/list-all-podcast.js
+++ bootcamp/src/views/list-all-podcast/list-all-podcast.js
@@ -1,3 +1,26 @@
+import { filterBySearchTerm } from '@/utils/filterBySearchTerm';
+import { getRandomPodcastCategory } from '@/utils/getRandomPodcastCategory';
+import api from '@/api';
+import TrackList from '@/components/track-list/track-list.vue';
+
 export default {
   name: 'ListAllPodcast',
+  components: {
+    TrackList,
+  },
+  data() {
+    return {
+      podcasts: [],
+      searchTerm: '',
+    };
+  },
+  computed: {
+    podcastList() {
+      return filterBySearchTerm(this.searchTerm, this.podcasts);
+    },
+  },
+  async mounted() {
+    this.podcasts = await api.search(this.$route.params.category || getRandomPodcastCategory());
+    this.podcastListFiltered = [...this.podcastList];
+  },
 };
