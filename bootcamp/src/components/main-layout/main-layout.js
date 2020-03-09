4b68e9d507ffc9ec241681f5ec4a6bef8ed3058a
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-03-09 11:13:23 -0500

    [BC-06] Refactor 'MainLayout' component

--- bootcamp/src/components/main-layout/main-layout.js
+++ bootcamp/src/components/main-layout/main-layout.js
@@ -1,10 +1,12 @@
 import AppBar from '@/components/app-bar/app-bar.vue';
 import MenuDrawer from '@/components/app-menu-drawer/app-menu-drawer.vue';
+import Player from '@/components/player/player.vue';
 
 export default {
   name: 'MainLayout',
   components: {
     AppBar,
     MenuDrawer,
+    Player,
   },
 };
