e967fa61821cfc7bb1de5b5b030dd2f21d70c457
Alejandro Benjumea <alejandro.benjumea@gorillalogic.com>
2020-03-09 11:43:30 -0500

    [BC-07] Add 'SearchInput' component

---
+++ bootcamp/src/components/search-input/search-input.vue
@@ -0,0 +1,2 @@
+<template src="./search-input.html" />
+<script src="./search-input.js" />
---
+++ bootcamp/src/utils/sanitizeText.js
@@ -0,0 +1,5 @@
+/**
+ * Sanitizes a given text
+ * @param {String} text
+ */
+export const sanitizeText = text => (text ? text.trim() : '');
